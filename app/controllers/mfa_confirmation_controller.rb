65b5b047e30e1da8e98bea17897769adb95ac3fa
kevinsmaster5 <kevin.masters@gsa.gov>
2024-02-21 14:08:03 -0500

    remove extra confirmation step for backup codes

--- app/controllers/mfa_confirmation_controller.rb
+++ app/controllers/mfa_confirmation_controller.rb
@@ -5,6 +5,9 @@ class MfaConfirmationController < ApplicationController
                                                    if: :backup_code_confirmation_needed?
 
   def show
+    if backup_code_confirmation_needed?
+      redirect_to confirm_backup_codes_path
+    end
     @content = mfa_confirmation_presenter
     analytics.user_registration_suggest_another_mfa_notice_visited
   end
